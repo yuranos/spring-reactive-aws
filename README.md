4f2db4773e5ba0eae1634452efcb41582e1103fd
Yura Nosenko <yuranos87@gmail.com>
2017-09-19 11:25:50 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-reactive-aws
+Will play with Spring 2.0 with Project Reactor, Groovy and AWS toolkit.
---
+++ src/main/groovy/com/yuranos/architecture/aws/AwsApplication.groovy
@@ -0,0 +1,12 @@
+package com.yuranos.architecture.aws
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AwsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AwsApplication, args
+	}
+}
