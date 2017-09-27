97dc898ef04962207c68d510c5101ac7308b44c6
Yura Nosenko <yuranos87@gmail.com>
2017-09-27 17:47:40 +0300

    Making JPA work!

--- src/main/groovy/com/yuranos/architecture/aws/relational/BookingRepository.groovy
+++ src/main/groovy/com/yuranos/architecture/aws/relational/BookingRepository.groovy
@@ -1,11 +1,18 @@
 package com.yuranos.architecture.aws.relational
 
 import com.yuranos.architecture.aws.rest.Booking
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 /**
  * Created by yuranos on 9/21/17.
  */
 interface BookingRepository extends CrudRepository<Booking, Long> {
 
+    @Modifying(clearAutomatically = true)
+    @Query('''update Booking b set b.passengerName = :#{#booking.passengerName}, b.destination = :#{#booking.destination},
+            b.destination = :#{#booking.destination} where b.id = :#{#booking.id}''')
+    int update(@Param("booking") Booking booking)
 }
--- src/main/groovy/com/yuranos/architecture/aws/rest/BookingsController.groovy
+++ src/main/groovy/com/yuranos/architecture/aws/rest/BookingsController.groovy
@@ -1,16 +1,20 @@
 package com.yuranos.architecture.aws.rest
 
 import com.yuranos.architecture.aws.relational.BookingRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Required
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import groovy.json.JsonBuilder
+
+import javax.validation.constraints.NotNull
 
 /**
  * Created by yuranos on 9/20/17.
@@ -19,29 +23,32 @@ import groovy.json.JsonBuilder
 @Newify([JsonBuilder])
 class BookingsController implements ErrorHandler {
 
+    @Autowired
     BookingRepository bookingRepository
 
-    @GetMapping("/booking")
-    String getBooking(@RequestParam(required=true) int id) {
+    @GetMapping("/booking/{id}")
+    String getBooking(@PathVariable(required = true) int id) {
 //        Booking(5, "Yura Nosenko", "Tenerife", new Date())
-        printf "Hello there!"
-        def booking = bookingRepository.find id
-        return JsonBuilder(booking).toPrettyString()
+        def booking = bookingRepository.findById(id)
+        return ResponseEntity.ok(booking)
     }
 
     @PostMapping("/booking")
     ResponseEntity<String> postBooking(@RequestBody Booking booking) {
-        return ResponseEntity.ok().body("The booking with id $booking.id has been created")
+        def createdBooking = bookingRepository.save(booking)
+        return ResponseEntity.ok("The booking with id $createdBooking.id has been created")
     }
 
     @DeleteMapping("/booking")
-    ResponseEntity<String> deleteBooking(int id) {
-        return ResponseEntity.ok().body("The booking with id ${id} has been deleted")
+    ResponseEntity<String> deleteBooking(@NotNull int id) {
+        bookingRepository.deleteById(id)
+        return ResponseEntity.ok("The booking with id ${id} has been deleted")
     }
 
     @PutMapping("/booking")
     ResponseEntity<String> putBooking(@RequestBody Booking booking) {
-                return ResponseEntity.ok().body("The booking with id $booking.id has been updated")
+        bookingRepository.update(booking)
+        return ResponseEntity.ok("The booking with id $booking.id has been updated")
     }
 
 //    @RequestMapping("/")
