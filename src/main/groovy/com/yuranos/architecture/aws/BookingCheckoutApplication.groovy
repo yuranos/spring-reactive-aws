59cffde3f7a30630e5eab1bb20219987f2de323d
Yura Nosenko <yuranos87@gmail.com>
2017-10-18 10:33:12 +0300

    Further clean up and RAML plugin addition

---
+++ src/main/groovy/com/yuranos/architecture/aws/BookingCheckoutApplication.groovy
@@ -0,0 +1,13 @@
+package com.yuranos.architecture.aws
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookingCheckoutApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BookingCheckoutApplication, args
+	}
+
+}
--- src/test/groovy/com/yuranos/architecture/aws/BookingCheckoutApplicationTests.groovy
+++ src/test/groovy/com/yuranos/architecture/aws/BookingCheckoutApplicationTests.groovy
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner)
 @SpringBootTest
-class AwsApplicationTests {
+class BookingCheckoutApplicationTests {
 
 	@Test
 	void contextLoads() {
